036e4543b5f4c3c6a89c6f61c2c55be69c0e65f3
pouya razavi <seyyedpwya@gmail.com>
2026-09-20 22:53:03 +0330

    overlay

--- js/account-bar.js
+++ js/account-bar.js
@@ -1,11 +1,19 @@
 const accountDrawer = document.getElementById("accountDrawer");
 const accountClose = document.getElementById("accountClose");
-const accountopen = document.getElementById("openAccount")
+const accountopen = document.getElementById("openAccount");
+const accountOverlay = document.getElementById("accountOverlay");
 
 accountClose.addEventListener("click" , function(){
     accountDrawer.classList.remove("active");
+    accountOverlay.classList.remove("active");
 });
 
 accountopen.addEventListener("click" , function(){
     accountDrawer.classList.add("active");
-})
+    accountOverlay.classList.add("active");
+});
+
+accountOverlay.addEventListener("click" , function(){
+    accountDrawer.classList.remove("active");
+    accountOverlay.classList.remove("active");
+});
